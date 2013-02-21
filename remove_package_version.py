50f2e7d741c3e8fc705f2b73ea06c357c729905a
Alberto Planas <aplanas@suse.com>
2013-02-21 11:49:15 +0100

    Add a script to remove the version name of a package.

---
+++ remove_package_version.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from get_path import PACKAGE
+from knapsack import read_file
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Group files in directories and add the size.')
+    parser.add_argument('file', help='File to remove the version name')
+
+    args = parser.parse_args()
+
+    items = read_file(args.file)
+
+    items_group = {}
+    for value, path in items:
+        m = PACKAGE.match(path)
+        path = m.groups()[0] if m else path
+        old_value = items_group.get(path, 0)
+        items_group[path] = old_value + value
+
+    for path, size in items_group.iteritems():
+        print size, path
