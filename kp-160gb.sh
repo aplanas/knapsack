0131feebd7b8414d5aea73d08107f188e9e19bb9
Alberto Planas <aplanas@suse.com>
2013-03-08 20:02:38 +0100

    Add 12.3 repo, increase the KP size.

--- kp-160gb.sh
+++ kp-160gb.sh
@@ -3,7 +3,7 @@
 KP=160
 
 # We need to reserve 21G for the repo and 16G for the ISOs 12.3
-KP=123
+KP=144
 
 # python setup.py build_ext --inplace
 python knapsack.py --price payload_final.txt --size no_version_disk_size_sql_with_dir.txt --wsize $(($KP*1024)) > kp-sol-$KP.txt
