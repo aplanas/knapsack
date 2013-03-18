4e00d47734875ba5fc5cb601ad3d594f6c84366b
Alberto Planas <aplanas@suse.com>
2013-03-18 09:06:01 +0100

    Usual KP sizes.

---
+++ kp-640g.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+KP=640
+
+# python setup.py build_ext --inplace
+python knapsack.py --price payload_final.txt --size no_version_disk_size_sql_with_dir.txt --wsize $(($KP*1024)) --fixsize > rsyncd-launch/${KP}g
+
+# python update-rsync.py --src /srv/ftp-stage/pub/opensuse/ --srcalt /srv/ftp/pub/opensuse/ --dst 160g/ kp-sol-160.txt
