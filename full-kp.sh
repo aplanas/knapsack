5bf4003ee2e713accc658e7c0cf5217a6e08fa04
Alberto Planas <aplanas@suse.com>
2013-03-21 11:25:03 +0100

    Fix the sort process.

--- full-kp.sh
+++ full-kp.sh
@@ -6,7 +6,7 @@
 # Compile with: python setup.py build_ext --inplace
 
 for KP in 30 80 160 320 640 ; do
-    python knapsack.py --price payload_final.txt --size no_version_disk_size_sql_with_dir.txt --wsize $(($KP*1024)) --fixsize > sort | rsyncd-launch/${KP}g
+    python knapsack.py --price payload_final.txt --size no_version_disk_size_sql_with_dir.txt --wsize $(($KP*1024)) --fixsize | sort > rsyncd-launch/${KP}g
 done
 
 RSYNC_PASSWORD=Boopah5g rsync -avz rsyncd-launch/* knapsacks@pontifex2-opensuse.suse.de::put-knapsacks
