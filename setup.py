a8f507295c4a0cdb2587073580cc63fbf6c9d8ee
Alberto Planas <aplanas@suse.com>
2013-08-28 11:09:32 +0200

    Reorder the project to simplify the package.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_module = Extension(
-    "knapsacklib",
-    ["knapsacklib.pyx"],
+    'knapsack.cknapsack',
+    ['knapsack/cknapsack.pyx'],
     extra_compile_args=['-fopenmp'],
     extra_link_args=['-fopenmp'],
 )
@@ -12,10 +12,15 @@ ext_module = Extension(
 setup(
     name='knapsack',
     version='1.0',
-    description='Knapsack alogrithm for openSUSE mirrors',
+    description='Knapsack algorithm for openSUSE mirrors',
     author='Alberto Planas',
     author_email='aplanas@suse.de',
+    py_modules=['knapsack'],
+    scripts=['disk_size_groups', 'kp', 'merge_payload',
+             'remove_package_version', 'remove_time_filter_new',
+             'get_file', 'run-when', 'update-rsync', 'disk_size',
+             'full-kp', 'mirror_brain', 'payload', 'prepare-kp',
+             'run-server', 'user-agents'],
     cmdclass={'build_ext': build_ext},
-    # ext_modules = [Extension("knapsacklib", ["knapsacklib.pyx"])]
     ext_modules=[ext_module],
 )
