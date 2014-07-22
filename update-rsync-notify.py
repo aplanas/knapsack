41a93d44e6cce23fd6b59359721fad5656d6afc3
Alberto Planas <aplanas@suse.com>
2014-07-22 12:39:48 +0200

    Make srcalt optional.

--- update-rsync-notify.py
+++ update-rsync-notify.py
@@ -12,11 +12,14 @@ from knapsack.utils import PACKAGE
 import pyinotify
 
 wm = pyinotify.WatchManager()  # Watch Manager
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_FROM # watched events
+mask = (pyinotify.IN_DELETE |
+        pyinotify.IN_CREATE |
+        pyinotify.IN_MOVED_FROM)  # watched events
 
 changed_dirs = dict()
 
 change_lock = threading.Lock()
+
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
@@ -29,14 +32,16 @@ class EventHandler(pyinotify.ProcessEvent):
         self.changed(event.pathname)
 
     def changed(self, pathname):
-        #print "Changed", os.path.dirname(pathname)
+        # print "Changed", os.path.dirname(pathname)
         change_lock.acquire()
-	changed_dirs[os.path.dirname(pathname)] = time.time()
+        changed_dirs[os.path.dirname(pathname)] = time.time()
         change_lock.release()
+
 
 handler = EventHandler()
 notifier = pyinotify.ThreadedNotifier(wm, handler, read_freq=10)
 notifier.start()
+
 
 def join(path, filename):
     return os.path.join(path, filename if filename[0] != '/' else filename[1:])
@@ -54,62 +59,69 @@ def get_files(path):
 
 
 _ALL_FILES = None
+
+
 def find_packages(_prefixname):
-    """Use the filename_prefix to find real packages. This assumes the prefices are sorted when called"""
+    """Use the filename_prefix to find real packages. This assumes the
+    prefices are sorted when called
+
+    """
     res = []
     global _ALL_FILES
     all_files = _ALL_FILES
     donewith = []
     found = False
     while len(all_files):
-	_basename = all_files.pop(0)
-	donewith.append(_basename)
-	if not _basename.startswith(_prefixname):
-            # we can skip everything if we already had a prefix hit and now are no longer 
-            if found: 
+        _basename = all_files.pop(0)
+        donewith.append(_basename)
+        if not _basename.startswith(_prefixname):
+            # we can skip everything if we already had a prefix hit and now are no longer
+            if found:
                 # but be careful - it might be the next prefix
                 all_files.insert(0, _basename)
                 return res
             continue
-	found = True
+        found = True
         _path = _basename
         m = PACKAGE.match(_basename)
-	if m: _path = m.groups()[0]
+        if m:
+            _path = m.groups()[0]
         if _path.endswith(_prefixname):
             res.append(_basename)
-	else:
-	    # if it's not matching, the path is longer so we can stop here
-	    all_files.insert(0, _basename)
-	    return res
+        else:
+            # if it's not matching, the path is longer so we can stop here
+            all_files.insert(0, _basename)
+            return res
     if not found:
-	_ALL_FILES = donewith
+        _ALL_FILES = donewith
     return res
+
 
 def expand_dir(_dirname, src_prefices):
     """Is called on initial setup of the all_dirs dict and later in the notify"""
     res = []
     global _ALL_FILES
     try:
-      af = []
-      for f in os.listdir(_dirname):
-	 af.append(f.replace('-32bit', '-THIRTYTWOBITS'))
-      _ALL_FILES = []
-      for f in sorted(af):
-        _ALL_FILES.append(f.replace('-THIRTYTWOBITS', '-32bit'))
-      
-    except: # if there is no such directory, we know the answer!!!
-      return res
+        af = []
+        for f in os.listdir(_dirname):
+            af.append(f.replace('-32bit', '-THIRTYTWOBITS'))
+        _ALL_FILES = []
+        for f in sorted(af):
+            _ALL_FILES.append(f.replace('-THIRTYTWOBITS', '-32bit'))
+
+    except:  # if there is no such directory, we know the answer!!!
+        return res
     if _dirname.endswith('/repodata'):
-	fp = _ALL_FILES
-	wanted_knaps = set()
-	for kps in src_prefices.values():
-  	  for kp in kps:
-	    wanted_knaps.add(kp)
-	#print "   ''", sorted(wanted_knaps), _ALL_FILES
-	return [['', sorted(wanted_knaps), _ALL_FILES]]
+        fp = _ALL_FILES
+        wanted_knaps = set()
+        for kps in src_prefices.values():
+            for kp in kps:
+                wanted_knaps.add(kp)
+        # print "   ''", sorted(wanted_knaps), _ALL_FILES
+        return [['', sorted(wanted_knaps), _ALL_FILES]]
     for prefix in sorted(src_prefices.keys()):
         fp = find_packages(prefix)
-	#print "  ", prefix, src_prefices[prefix], fp
+        # print "  ", prefix, src_prefices[prefix], fp
         res.append([prefix, src_prefices[prefix], fp])
     return res
 
@@ -128,11 +140,11 @@ if __name__ == '__main__':
         exit(0)
 
     args.src = os.path.abspath(args.src)
-    args.srcalt = os.path.abspath(args.srcalt)
+    args.srcalt = os.path.abspath(args.srcalt) if args.srcalt else None
     src_files_to_expand = dict()
     for kpsol in args.kpsol:
-	kp_files = sorted(f.strip() for f in open(join(args.lists, kpsol)) if f.strip())
-	for file in kp_files:
+        kp_files = sorted(f.strip() for f in open(join(args.lists, kpsol)) if f.strip())
+        for file in kp_files:
             fte = src_files_to_expand.get(file, [])
             src_files_to_expand[file] = fte.append(kpsol)
 
@@ -143,12 +155,12 @@ if __name__ == '__main__':
         _filename = join(args.src, src_file)
         _dirname, _prefixname = os.path.split(_filename)
         if _dirname != currentdir:
-	   if currentdir: 
-	     wm.add_watch(currentdir, mask)
-             #print currentdir
-	     all_dirs[currentdir[len(args.src):]] = expand_dir(currentdir, srcfiles)
-           currentdir = _dirname
-	   srcfiles = dict()
+            if currentdir:
+                wm.add_watch(currentdir, mask)
+                # print currentdir
+                all_dirs[currentdir[len(args.src):]] = expand_dir(currentdir, srcfiles)
+            currentdir = _dirname
+            srcfiles = {}
         srcfiles[_prefixname] = src_files_to_expand[src_file]
 
     args.dst = os.path.abspath(args.dst)
@@ -220,15 +232,17 @@ if __name__ == '__main__':
             try:
                 os.link(src, dst)
             except:
-                #print 'SRC', src
-                #print 'DST', dst
-                src = join(args.srcalt, f)
-                try:
-                   os.link(src, dst)
-                except:
-                   # the initial startup is slow, so it happens that files disappear
-                   # but once we're done here, we look at the notify events
-                   pass 
+                # print 'SRC', src
+                # print 'DST', dst
+                if args.srcalt:
+                    src = join(args.srcalt, f)
+                    try:
+                        os.link(src, dst)
+                    except:
+                        # the initial startup is slow, so it happens that
+                        # files disappear but once we're done here, we look
+                        # at the notify events
+                        pass
 
         # print '** Del files'
         # for i in del_files:
@@ -247,13 +261,14 @@ if __name__ == '__main__':
         for d in sorted(del_dir, reverse=True):
             d = join(kpdir, d)
             os.rmdir(d)
- 
+
     print "looping"
-    while not changed_dirs.has_key(args.lists):
+    while args.lists not in changed_dirs:
         dirtoupdate = None
         change_lock.acquire()
         for dir in changed_dirs:
-            if time.time() - changed_dirs[dir] < 100: continue
+            if time.time() - changed_dirs[dir] < 100:
+                continue
             del changed_dirs[dir]
             dirtoupdate = dir[len(args.src):]
             break
@@ -262,19 +277,19 @@ if __name__ == '__main__':
             time.sleep(20)
             continue
         # update_dir(dirs)
-      
-        if not all_dirs.has_key(dirtoupdate):
+
+        if dirtoupdate not in all_dirs:
             print "not found:", dirtoupdate, sorted(all_dirs.keys())
             continue
 
         print "updating", dirtoupdate
-        #print "B:", dirtoupdate, all_dirs[dirtoupdate]
+        # print "B:", dirtoupdate, all_dirs[dirtoupdate]
         # collect the prefices in all knapsacks
         srcfiles = dict()
         for src_file in all_dirs[dirtoupdate]:
             srcfiles[src_file[0]] = src_file[1]
         all_dirs[dirtoupdate] = expand_dir(join(args.src, dirtoupdate), srcfiles)
-        #print "A:", dirtoupdate, all_dirs[dirtoupdate]
+        # print "A:", dirtoupdate, all_dirs[dirtoupdate]
 
         # update knapsacks
         for kpsol in args.kpsol:
@@ -287,7 +302,7 @@ if __name__ == '__main__':
             dst_files, dst_dir = get_files(kpdir)
             new_files = src_files - dst_files
             del_files = dst_files - src_files
-            #print kpsol, src_files, dst_files, new_files, del_files
+            # print kpsol, src_files, dst_files, new_files, del_files
             # TODO: avoid code duplication
             if del_files:
                 print >> sys.stderr, '{1}: Unlinking deleted files ({0})...'.format(len(del_files), kpsol)
@@ -302,13 +317,14 @@ if __name__ == '__main__':
                 try:
                     os.link(src, dst)
                 except:
-                    #print 'SRC', src
-                    #print 'DST', dst
-                    src = os.path.join(join(args.srcalt, dirtoupdate), f)
-                    try:
-                        os.link(src, dst)
-                    except:
-                        print "linking failed", f, dirtoupdate, kpsol
-                        pass
+                    # print 'SRC', src
+                    # print 'DST', dst
+                    if args.srcalt:
+                        src = os.path.join(join(args.srcalt, dirtoupdate), f)
+                        try:
+                            os.link(src, dst)
+                        except:
+                            print "linking failed", f, dirtoupdate, kpsol
+                            pass
 
     notifier.stop()
